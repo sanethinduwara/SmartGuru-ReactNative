9ed647ebc857360fbca5654e1728bf7d46c367d7
sanethinduwara <47098118+sanethinduwara@users.noreply.github.com>
2019-05-27 00:36:24 +0530

    Update stackoverflowrecommendations.js

--- src/screens/stackoverflowrecommendations.js
+++ src/screens/stackoverflowrecommendations.js
@@ -5,7 +5,7 @@ import {
     WebView,
     Text,
     ActivityIndicator,
-    Linking
+    Linking, AsyncStorage
 
 } from 'react-native';
 
@@ -22,30 +22,46 @@ export default class StackOverFlowRecommendations extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isLoading: true
+            isLoading: true,
+            userID: ''
         }
     }
 
 
     componentDidMount() {
 
-        const URL = `http://smartguru-env.mfrzh7c8xs.us-east-1.elasticbeanstalk.com/stackoverflowlinks`;
-        console.log("component did mount");
-        return fetch(URL)
-            .then((response) => response.json())
-            .then((responseJson) => {
-                this.links = responseJson;
-                console.log("links", responseJson.toString());
-                this.setState({
-                    isLoading: false,
-                }, function () {
+        (async () => {
+            await this.setUserID();
 
+            const URL = `http://smartguru-env.mfrzh7c8xs.us-east-1.elasticbeanstalk.com/stackoverflowlinks/${this.state.userID}`;
+
+            return fetch(URL)
+                .then((response) => response.json())
+                .then((responseJson) => {
+                    this.links = responseJson;
+                    console.log("links", responseJson.toString());
+                    this.setState({
+                        isLoading: false,
+                    }, function () {
+
+                    });
+                })
+                .catch((error) => {
+                    console.error(error);
                 });
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+        })();
+
+
     }
+
+    setUserID = async () => {
+
+        const userID = await AsyncStorage.getItem('@userID');
+
+        this.setState({
+            userID: userID
+        });
+    };
 
     displayLinks = () => {
         return this.links.map((link, index) => {
